689cfb5ced08cd7602a7ad29466b2dfbfddd47b9
lhl <lhl@hwc08.cin.ufpe.br>
2025-11-05 10:21:50 -0300

    feat(client_node): initial version of the client node

--- ros2_robotic_arm/src/robot_arm/robot_arm/robot_planner.py
+++ ros2_robotic_arm/src/robot_arm/robot_arm/robot_planner.py
@@ -2,32 +2,82 @@ import sys
 import rclpy 
 import matplotlib.pyplot as plt
 from rclpy.node import Node
-from trajectory_msgs.msg import JointTrajectory 
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from std_msgs.msg import Float64
+from arm_interfaces.srv import FollowTrajectory
+
 
 import numpy as np
 from math import sin, cos, sqrt
+from .utils import TrajectoryPlanner
 
 
-def main():
-    print('Hi from planner.')
-
-if __name__ == '__main__':
-    main()
+MAX_JOINT_VELOCITY = 1 # RAD/s
+MAX_JOINT_ACCELERATION = 1 # RAD/s²
+L1 = 10.3
+L2 = 12.28
+L3 = 5.2
 
 
 class MinimalClientAsync ( Node ) :
 
     def __init__ ( self ) :
         super () . __init__ ('minimal_client_async')
-        self.cli = self.create_client (JointTrajectory, 'robot_planner')
+        self.cli = self.create_client (FollowTrajectory, 'robot_planner')
+        self.trajectoryPlanner = TrajectoryPlanner(MAX_JOINT_VELOCITY, MAX_JOINT_ACCELERATION, L1, L2, L3)
         while not self.cli.wait_for_service(timeout_sec =1.0):
           self.get_logger().info ('waiting for service ...')
-          self.req = JointTrajectory
-    def send_request (self, a, b):
-        self.req.a = a
-        self.req.b = b
-        return self.cli.call_async(self.req)
+          self.req = FollowTrajectory.Request()
     
+    def move(self, position: np.ndarray) -> None:
+        """
+        Input: position in task space [x,y,z,phi]
+        """
+
+        points = self.trajectoryPlanner.move(position)
+        #(theta1, theta2, theta3, theta4), (theta11, theta21)
+        
+        jointTrajectory = JointTrajectory()
+        jointTrajectoryPoints = []
+       
+       
+        for point in points:
+            jointTrajectoryPoint = JointTrajectoryPoint()
+            jointTrajectoryPoint.positions.append(Float64(point[0]))
+            jointTrajectoryPoint.positions.append(Float64(point[1]))
+            jointTrajectoryPoint.positions.append(Float64(point[2]))
+            jointTrajectoryPoint.positions.append(Float64(point[3]))
+            jointTrajectoryPoints.append(jointTrajectoryPoint)
+        
+        jointTrajectory.points = jointTrajectoryPoints
+
+        self.req.trajectory.points = jointTrajectory
+        return self.cli.call_async(self.req)
+
+
+
+def main():
+    x, y, z, fi = sys.argv[1:5]
+    position_aim = np.array([x, y, z, fi], np.float32)
+    
+    
+    rclpy.init()
+    cliente = MinimalClientAsync()
+    future = cliente.move(position_aim)
+    rclpy.spin_until_future_complete(cliente, future)
+    response = future.result()
+
+    cliente.destroy_node()
+    rclpy.shutdown()
+    
+
+    
+if __name__ == "__main__":
+    main()
+             
+          
+
+       
     
 
 
